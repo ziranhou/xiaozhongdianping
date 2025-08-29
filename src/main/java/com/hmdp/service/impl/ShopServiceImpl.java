0610d7519aec92b5360587c154764d3bb2c7b284
houziran <2823483451@qq.com>
2025-08-29 22:09:11 +0800

    使用互斥锁解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -34,19 +35,84 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        // 缓存穿透，调用封装好的方法queryWithPassThrough()
+        // Shop shop = queryWithPassThrough(id);
+
+        // 互斥锁：解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if (shop == null){
+            return Result.fail("店铺不存在");
+        }
+        // 7. 返回
+        return Result.ok(shop);
+    }
+
+    // 互斥锁解决缓存击穿，封装为queryWithMutex（）方法
+    public Shop queryWithMutex(Long id) {
         String key = CACHE_SHOP_KEY + id;
         // 1. 从Redis中查询商铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(key);
         // 2. 判断是否存在
         if (StrUtil.isNotBlank(shopJson)){
             // 3. 存在，返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSONUtil.toBean(shopJson, Shop.class);
         }
         // 判断命中的是否是空值
         if (shopJson != null){
             // 返回错误信息
-            return Result.fail("店铺不存在！");
+            return null;
+        }
+
+        // 4. 解决缓存击穿：实现缓存重建
+        // 4.1 获取互斥锁
+        String lockkey = LOCK_SHOP_KEY + id;
+        Shop shop = null;
+        try {
+            boolean islock = trylock(lockkey);
+            // 4.2 判断是否获取成功
+            if (!islock){
+                // 4.3 如果失败，休眠并重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+            // 4.4 成功，根据id查询数据库
+            shop = getById(id);
+            // 模拟重建的延时
+            Thread.sleep(200);
+            // 5. 不存在，返回错误
+            if (shop ==  null) {
+                // 将空值缓存（写入redis），解决缓存穿透
+                stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+                // 返回错误信息
+                return null;
+            }
+            // 6. 存在，写入Redis
+            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }finally {
+            // 7. 释放互斥锁
+            unLock(lockkey);
+        }
+        // 8. 返回
+        return shop;
+    }
+
+
+    // 封装好了的缓存穿透代码（上述）
+    public Shop queryWithPassThrough(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        // 1. 从Redis中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2. 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)){
+            // 3. 存在，返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        // 判断命中的是否是空值
+        if (shopJson != null){
+            // 命中空值，说明数据库中也不存在该商铺，直接返回null
+            return null;
         }
         // 4. 不存在，根据id查询数据库
         Shop shop = getById(id);
@@ -55,12 +121,22 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             // 将空值缓存（写入redis），解决缓存穿透
             stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             // 返回错误信息
-            return Result.fail("店铺不存在！");
+            return null;
         }
         // 6. 存在，写入Redis
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 7. 返回
-        return Result.ok(shop);
+        return shop;
+    }
+
+    // 获取锁和释放锁
+    private boolean trylock(String key) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unLock(String key) {
+        stringRedisTemplate.delete(key);
     }
 
     @Override
